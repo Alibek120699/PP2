2671d3fdf9ab7cb50be4e8682cd9e1ae41a4734b
Alibek120699 <sayakalibek1@gmail.com>
2018-01-28 03:54:29 +0600

    second

---
+++ Lab2/Lab2/Complex.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Complex
+    {
+        int a;
+        int b;
+        public Complex(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public static int Gcd(int a, int b)
+        {
+            if (b == 0) return a;
+            else return Gcd(b, a % b);
+        }
+        public static int Lcm(int a, int b)
+        {
+            int max = Math.Max(a, b);
+            for(int i=max; ; i++)
+            {
+                if(i%a==0 && i % b == 0)
+                {
+                    max = i;
+                    break;
+                }
+            }
+            return max;
+        }
+        public void Simplify()
+        {
+            int a1 = a;
+            int b1 = b;
+            int div = Gcd(a1, b1);
+            this.a = this.a / div;
+            this.b = this.b / div;
+        }
+        public static Complex operator *(Complex x, Complex y)
+        {
+            Complex result1 = new Complex(x.a * y.a, x.b * y.b);
+            result1.Simplify();
+            if (result1.b < 0)
+            {
+                result1.b = result1.b * (-1);
+                result1.a = result1.a * (-1);
+            }
+            return result1;
+        }
+        public static Complex operator /(Complex x, Complex y)
+        {
+            Complex result2 = new Complex(x.a * y.b, x.b * y.a);
+            result2.Simplify();
+            if (result2.b < 0)
+            {
+                result2.b = result2.b * (-1);
+                result2.a = result2.a * (-1);
+            }
+            return result2;
+        }
+        public static Complex operator +(Complex x, Complex y)
+        {
+            int lcm = Lcm(x.b, y.b);
+            Complex result3 = new Complex((lcm / x.b) * x.a + (lcm / y.b) * y.a, lcm);
+            result3.Simplify();
+            if (result3.b < 0)
+            {
+                result3.b = result3.b * (-1);
+                result3.a = result3.a * (-1);
+            }
+            return result3;
+        }
+        public static Complex operator -(Complex x, Complex y)
+        {
+            int lcm = Lcm(x.b, y.b);
+            Complex result4 = new Complex((lcm / x.b) * x.a - (lcm / y.b) * y.a, lcm);
+            result4.Simplify();
+            if (result4.b < 0)
+            {
+                result4.b = result4.b * (-1);
+                result4.a = result4.a * (-1);
+            }
+            return result4;
+        }public override string ToString()
+        {
+            return a + "/" + b;
+        }
+    }
+}
---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] cmpx = s.Split(' ');
+            string[] cmp1 = cmpx[0].Split('/');
+            string[] cmp2 = cmpx[1].Split('/');
+            
+            int i1 = int.Parse(cmp1[0]);
+            int i2 = int.Parse(cmp1[1]);
+            int i3 = int.Parse(cmp2[0]);
+            int i4 = int.Parse(cmp2[1]);
+            Complex c1 = new Complex(i1, i2);
+            Complex c2 = new Complex(i3, i4);
+            Complex  mul = c1 * c2;
+            Complex  div = c1 / c2;
+            Complex  add = c1 + c2;
+            Complex  sub = c1 - c2;
+            Console.WriteLine("The multiplication of these two numbers is: " + mul);
+            Console.WriteLine("The division of these two numbers is:  " + div);
+            Console.WriteLine("The addition of these two numbers is: " + add);
+            Console.WriteLine("The substraction of these two numbers is: " + sub);
+        }
+    }
+}
