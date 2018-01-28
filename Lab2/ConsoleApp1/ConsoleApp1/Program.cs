75d01ccb3157f9d9f1bff8297c58e841dbefed07
Alibek120699 <sayakalibek1@gmail.com>
2018-01-28 20:16:15 +0600

    second2

---
+++ Lab2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static bool Prime (int n)
+        {
+            bool ok = true;
+
+            if (n < 2)
+            {
+                ok = false;
+            }
+            else
+            {
+                for (int i = 2; i <= Math.Sqrt(n); i++)
+                {
+
+                    if (n % i == 0)
+                    {
+                        ok = false;
+                        break;
+                    }
+                }
+            }
+            if (ok == false)
+                return false;
+            return true;
+            
+        }
+        
+        static void Main(string[] args)
+        {
+            
+            string path = @"C:\Users\HP\Desktop\PP2\Lab2\input.txt";
+            string lines = File.ReadAllText(path);
+            string[] numbers = lines.Split(' ');
+            int min = 1000;
+            int max = 1;
+            List<int> primes = new List<int>();
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if(Prime(int.Parse(numbers[i]))== true)
+                {
+                    primes.Add(int.Parse(numbers[i]));
+                }
+            }
+            for(int i = 0; i < primes.Count; ++i)
+            {
+                if (primes[i] > max)
+                {
+                    max = primes[i];
+                }
+                if (primes[i] < min)
+                {
+                    min = primes[i];
+                }
+            }
+            Console.WriteLine("min: " + min);
+            Console.WriteLine("max: " + max);
+            string[] mins = { "min prime: " + Convert.ToString(min)," ", "max prime: " + Convert.ToString(max) };
+            
+            File.WriteAllLines(@"C:\Users\HP\Desktop\PP2\Lab2\output.txt", mins);
+            
+        }
+    }
+}
---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // You can also use System.Environment.GetLogicalDrives to
+            // obtain names of all logical drives on the computer.
+            DriveInfo di = new DriveInfo(@"C:\");
+            
+            
+        }
+    }
+}
